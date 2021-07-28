d405fafc2864c88219486d10b90a5719cdb9d769
Guillermo Antony Cava Nuñez <gcavanunez@gmail.com>
2021-07-28 17:27:18 -0500

    learn completE

---
+++ pages/posts/first-post.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Image
+        src="/images/g-and-c.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
+      <h2>
+        <Link href="/">
+          <a className="foo" target="_self" rel="noopener noreferrer">
+            Back to home
+          </a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
